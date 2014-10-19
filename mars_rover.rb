fda753c06eccfc0e9b7fabd319bc1d9ae5e8c2e0
Alex Windett <alexwindett@hotmail.com>
2014-10-19 17:56:23 +0100

    checking for valid data

--- mars_rover.rb
+++ mars_rover.rb
@@ -18,7 +18,18 @@ answer = menu
         size = gets.chomp
         mars_dimensions = size.split()
 
-      @mars =  Mars.new(mars_dimensions[0], mars_dimensions[1])
+      if
+         # making sure that the data provided is correct
+        mars_dimensions.empty?
+          puts "please provide co-ordinates"
+      elsif 
+        binding.pry
+        mars_dimensions[0] > 50 || mars_dimensions[1] > 50 || mars_dimensions[0] < 0 || mars_dimensions[1] < 0
+          puts "Please provide valid co-ordinates (0 - 50)"
+      else
+        @mars =  Mars.new(mars_dimensions[0].to_i, mars_dimensions[1].to_i)
+      end
+
     elsif answer == "2"
       puts "What are the robots Co-ords and Orientation (e.g. 1 1 e)"
         info = gets.chomp
@@ -26,11 +37,23 @@ answer = menu
       puts "Enter instructions for your robot"
         robot_instructions = gets.chomp.upcase
       
-      robot = Robot.new(robot_data[0].to_i, robot_data[1].to_i, robot_data[2].capitalize, robot_instructions).action
+        # making sure that the data provided is correct
+        if robot_data[0].empty? || robot_data[1].empty? || robot_data[2].empty?
+          puts "please provide valid coordinates and direction"
+        elsif 
+          robot_instructions.empty?
+            puts "Please provide instructions"
+        elsif 
+          robot_instructions.split(//).count > 100 || robot_instructions.split(//).count < 0
+          puts "Please give instructions greater than 0 but less than 100"
+        else
+          robot = Robot.new(robot_data[0].to_i, robot_data[1].to_i, robot_data[2].capitalize, robot_instructions).action
+          @mars.robots << robot
+        end
 
-      @mars.robots << robot
     else
-      puts "Houston we have a problem"
+      puts "Houston we have a problem, please try again"
     end
+
   answer = menu
   end
